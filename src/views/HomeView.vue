df71d75776ff178202e4e975ed0756cc295497bc
T0m4s1n <tomas.benavidesc@campusucc.edu.co>
2024-12-19 00:26:47 -0500

    fixed for build

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,18 @@ import HeroComponent from '../components/HeroComponent.vue';
 import PagesComponent from '@/components/PagesComponent.vue';
 import AboutComponent from '@/components/AboutComponent.vue';
 import ContactComponent from '@/components/ContactComponent.vue';
+import profileImage from '../assets/t0m4s1n.png';
 </script>
 
 <template>
   <div>
     <section id="hero">
-      <HeroComponent />
+      <HeroComponent
+        name="T0m4s1n"
+        title="Portfolio"
+        description="Welcome to my portfolio. I am a full-stack developer with a passion for creating beautiful and functional websites."
+        :profileImage="profileImage"
+      />
     </section>
 
     <section id="projects">
